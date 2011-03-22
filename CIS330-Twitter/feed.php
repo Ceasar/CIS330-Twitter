31898516ab583a4adf31fff3803dee53024fc76a
Cris Feo <svgreaser@gmail.com>
2011-03-21 20:11:55 -0400

    Did a LOT of stuff. - I think the databasetools.php should hold useful functions   related to querying. I put in a function that posts tweets   as an example. - Also, I updated the db schema because the datatype for   tweet privacy wasn't a bool - There is a new php file called newTweetWidget.php. This   contains a function which, when called puts a element on   the page that a user can use to post a tweet. It is used   on the newsfeed page at the bottom.
    Signed-off-by: Cris Feo <svgreaser@gmail.com>

--- CIS330-Twitter/feed.php
+++ CIS330-Twitter/feed.php
@@ -1,14 +1,20 @@
 <?php
-	include_once("./databaseTools.php");
+//Includes:
+include_once("./databaseTools.php");
+include_once("./newTweetWidget.php");
+
+//DEBUG
+$_SESSION['username'] = "userA"; //Fake a session
 ?>
 
 <html>
 
 <head>
-	<title>Twitter 
+	<!-- This makes the title display the username if the client is logged in -->
+	<title>Twitter - News Feed
 		<?php
 			if ( isset($_SESSION['username']) ) {
-				echo $_SESSION['username'];
+				echo " - " . $_SESSION['username'];
 			}
 		?>
 	</title>
@@ -20,22 +26,27 @@
 	</div>
 	
 	<div id="content">
-		<h2>News Feed:</h2>
-		<ul id="newsList">
-			<?php
-				//Query the db for all tweets/PMs related to the current user
-				$query = "SELECT users.id as usr, tweets.message as msg\n"
-					   . "FROM users, tweeted, tweets\n"
-					   . "WHERE users.id=tweeted.userid and tweeted.tid=tweets.id";
-				$result = run_sql($query);
-				//Loop through the set of tweets
-				while ( $row=mysql_fetch_array($result) ) {
-					?>
-					<li> @<?php echo $row['usr'] ?> tweeted "<?php echo $row['msg'] ?>" </li>
-					<?php
-				}
-			?>
-		</ul>
+	
+		<div id="newsFeed">
+			<h2>News Feed:</h2>
+			<ul id="newsList">
+				<?php
+					//Query the db for all tweets/PMs related to the current user
+					$query = "SELECT users.id as usr, tweets.message as msg\n"
+						   . "FROM users, tweeted, tweets\n"
+						   . "WHERE users.id=tweeted.userid and tweeted.tid=tweets.id";
+					$result = run_sql($query);
+					//Loop through the set of tweets
+					while ( $row=mysql_fetch_array($result) ) {
+						echo "<li>@". $row['usr'] ." tweeted ". $row['msg'] ."</li>";
+					}
+				?>
+			</ul>
+		</div>
+		
+		<!--  This draws a widget for submitting tweets. Exposed by 'newTweetWidget.php' -->
+		<?php addTweetWidget(); ?>
+		 
 	</div>
 </body>
 
---
+++ CIS330-Twitter/newTweetWidget.php
@@ -0,0 +1,93 @@
+<?php
+/* New Tweet Widget
+ * ================
+ * Description: 
+ *  Include this script on your page then call 'addTweetWidget()' wherever you want this in your page.
+ */
+
+//Includes:
+include_once("./databaseTools.php");
+
+
+
+//This function draws the whole widget
+//------
+function addTweetWidget() {
+	//Put the div/header for this widget on the page
+	?>
+	<div class='newTweet'>
+	<h2>Create Tweet:</h2>
+	<?php
+	
+	//See if the user is even logged in
+	if ( !isset($_SESSION['username']) ) {
+		echo "You must be logged in to post tweets.<br/>";
+	} else {
+		//The page self-submits so if it submitted, try to post the tweet
+		$tweetDefault = "Write your tweet here. (Max 140 characters)";
+		if (isset($_POST['submit'])) {
+			$result = submitTweet();
+			//If the user input was bad (e.g. too long), allow them to fix it 
+			if (!$result) {
+				createPostForm($_POST['message'], false);
+				echo "Tweet was too long, try again. (max of 140 characters)<br/>";
+			} else {
+				//If it submitted correctly, display an new blank form
+				createPostForm($tweetDefault);
+			}
+		} else {
+			//If we haven't submitted, display an new blank form
+			createPostForm($tweetDefault);
+		}
+	}
+	
+	//Close out the container element for this widget
+	echo "</div>";
+}
+
+//This draws the form element for the widget. The argument determines the initial content.
+//------
+function createPostForm($text, $default=true) {
+	?>
+	<form method="post" action="<?php $PHP_SELF;?>">
+		<textarea rows="10" cols="30" name="message" onClick="clearText(event)" onKeyDown="changeText(event)"><?php echo $text ?></textarea><br/>
+		Private? <input type="checkbox" value="1" name="privacy"/>
+		<input type="submit" value="Post" name="submit">
+		Remaining:<input readonly type="text" id="lenCount" size=3 maxlength=3 value=<?php echo 140-(strlen($text)+1) ?>>
+	</form>
+	<script type="text/javascript">
+		function clearText(e) {
+			if (e.target.value=='<?php echo ($default?$text:"") ?>') {
+				e.target.value = "";
+				document.getElementById("lenCount").value = 0;
+			}
+		}
+		function changeText(e) {
+			document.getElementById("lenCount").value = 140-(e.target.value.length+1);
+		}
+	</script>
+	<?php
+}
+
+// Attempts to submit the result of the form submission. Returns: Bool whether or not the input was valid
+//------
+function submitTweet() {
+	//Get the submitted values
+	$msg = $_POST['message'];
+	$usr = ($_SESSION['username']);
+	
+	//Determine privacy setting from checkbox
+	$priv = false;
+	if(isset($_POST['privacy']) && $_POST['privacy'] == '1') {
+		$priv = true;
+	}
+	
+	//Try to insert the message into the db and return whether the insertion was successful
+	$result = db_addTweet($usr, $msg, $priv);
+	if ($result) {
+		echo "@".$usr." ".($priv?"privately":"")." tweeted '".$msg."'<br/>";
+	}
+	return $result;
+}
+
+?>
