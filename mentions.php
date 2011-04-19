13ac3fe75d4176b7c50aed1f4b6ba286f0027c4a
Paul Przybysz <pprzy@seas.upenn.edu>
2011-04-18 22:27:29 -0400

    Added mentions page

---
+++ mentions.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+/* Mentions page
+ * 
+ */
+
+//Includes:
+include_once("./databaseTools.php");
+include_once("./newTweetWidget.php");
+
+//DEBUG:
+//$_SESSION['userid'] = "userA"; //Fake a session
+
+function displayMentionedTweets() {
+	//Query the db for all tweets
+    $query = "SELECT users.id as usr, tweets.message as msg\n"
+                . "FROM users, tweeted, tweets\n"
+                . "WHERE users.id=tweeted.userid and tweeted.tid = tweets.id";
+    $result = run_sql($query);
+    //Loop through, pick out as necessary
+    $mention = "@" . $_SESSION['userid'];
+    while( $row=mysql_fetch_array($result) ){
+        
+        $pos = strpos($row['msg'],$mention);
+        if($pos === false) {
+            // mention NOT found in message
+            // do nothing, skip
+            ;
+        }
+        else {
+            // mention found in message
+            //echo "<li>@". $row['usr'] ." tweeted ". $row['msg']."</li>";
+        }
+        
+    }
+}
+
+?>
+
+<html>
+
+<?php include_once("./assets/templates/head.php");?>
+
+<body>
+
+<?php include_once("./assets/templates/header.php");?>
+
+<!-- Unique page content -->
+<div id="content">
+    <ul id="mentionList">
+        <?php displayMentionedTweets(); ?>
+    </ul>
+    
+    <?php addTweetWidget(); ?>
+</div>
+
+<?php include_once("./assets/templates/footer.php");?>
+
+</body>
+</html>
