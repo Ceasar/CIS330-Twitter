3fb9a7b99727277f1cb5c7b4c2dc8433e38664b0
wellecks <wellecks@gmail.com>
2011-04-18 20:21:26 -0400

    search.php

---
+++ search.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+include_once("./databaseTools.php");
+
+function execute_search(){
+	// Get the search variable from URL
+	$var = @$_GET['search_text'] ;
+	$trimmed = trim($var); //trim whitespace from the stored variable
+
+	// rows to return
+	$limit=10;
+
+	// check for an empty string or no search term.
+	if (($trimmed == "") || (!isset($var))){
+		exit;
+	}
+
+	$result = db_searchForUser($trimmed);
+	
+	while ($row=mysql_fetch_array($result)) {
+		echo $row['last_name'] . "\n";
+	}
+}
+?>
+
+<html>
+<?php include_once("./assets/templates/head.php");?>
+
+<body>
+
+<?php include_once("./assets/templates/header.php");?>
+
+<!-- Unique page content goes here. -->
+<div id="content">
+	<?php execute_search();?>
+</div> <!-- End Content Div -->
+
+
+<?php include_once("./assets/templates/footer.php");?>
+
+</body>
+</html>
+
